9750401fcad76c4aae51f86a146b6088f4b0c040
hedysom <yevgeniy.terentyev@gmail.com>
2025-07-29 22:21:19 +0200

    projectiles

--- index.js
+++ index.js
@@ -1,149 +1,214 @@
-const canvas = document.querySelector("canvas")
-const c = canvas.getContext("2d")
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Player {
-    constructor(params) {
-        const image = new Image()
-        image.src = "img/spaceship.png"
-        
-        image.onload = () => {
-            const scale = 0.15
-            this.image = image
-            this.width = image.width * scale
-            this.height = image.height * scale
+  constructor(params) {
+    const image = new Image();
+    image.src = "img/spaceship.png";
 
-            //position the player at the bottom at half screen
-            this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - this.height - 10
-            }
-        }
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
 
-        this.rotation = 0
+      //position the player at the bottom at half screen
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 10,
+      };
+    };
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+    this.rotation = 0;
 
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+  }
 
+  draw() {
+    //c.fillStyle = "red"
+    //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    //rotation of the image in a very unintuitive way
+    //the canvas is translated to the position of the player and then rotated on press
+    c.save();
+    c.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    );
+    c.rotate(player.rotation);
+
+    //postion the canvas to back to it's original state
+    c.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    );
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+
+    //restore the previously saved state
+    c.restore();
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
     }
-
-    draw() {
-        //c.fillStyle = "red"
-        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        
-        //rotation of the image in a very unintuitive way
-        //the canvas is translated to the position of the player and then rotated on press
-        c.save()
-        c.translate(player.position.x + player.width/2, player.position.y + player.height/2)
-        c.rotate(player.rotation)
-
-        //postion the canvas to back to it's original state
-        c.translate(- player.position.x - player.width/2, -player.position.y - player.height/2)
-
-        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-        
-        //restore the previously saved state
-        c.restore()
-    }
-
-    update() {
-        if(this.image){
-            this.draw()
-            this.position.x += this.velocity.x
-        }
-    }
-
+  }
 }
 
-const player = new Player()
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.radius = 3;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+    c.strokeStyle = "red";
+    c.stroke();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+const player = new Player();
+const projectiles = [];
 
 //currect status on keys
 const keys = {
-    a : {
-        pressed : "false"
-    },
-    s : {
-        pressed : "false"
-    },
-    d : {
-        pressed : "false"
-    },
-    w : {
-        pressed : "false"
-    }
-}
+  a: {
+    pressed: "false",
+  },
+  s: {
+    pressed: "false",
+  },
+  d: {
+    pressed: "false",
+  },
+  w: {
+    pressed: "false",
+  },
+  space: {
+    pressed: "false",
+  },
+};
 
-//animate in loop (otherwise the image is not drawn at all, draw function is called 
+//animate in loop (otherwise the image is not drawn at all, draw function is called
 // before the immage loads)
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-    //background 
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    c.fillStyle = "black"
+  //background
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "black";
 
-    //draw the player
-    player.update()
+  //draw the player
+  player.update();
 
-    //check for a and d to be pressed, other conditions are for the player to not go
-    //outside the screen
-    if(keys.a.pressed == true && player.position.x >= 0){
-        player.velocity.x = -7
-        player.rotation = -0.15
+
+  //array with projectiles
+  projectiles.forEach((projectile, index) => {
+
+    //remove projectiles if they go out of the screen
+    if (projectile.position.y <= 0) {
+      projectiles.splice(index, 1);
+      //otherwise update the ones still on the screen
+    } else {
+      projectile.update();
     }
-        else if(keys.d.pressed == true && player.position.x + player.width <= canvas.width){
-                player.velocity.x = 5
-                player.rotation = 0.15
-            }
-                else{
-                    player.velocity.x = 0
-                    player.rotation = 0
-                }
+  });
+
+  //check for a and d to be pressed, other conditions are for the player to not go
+  //outside the screen
+  if (keys.a.pressed == true && player.position.x >= 0) {
+    player.velocity.x = -7;
+    player.rotation = -0.15;
+  } else if (
+    keys.d.pressed == true &&
+    player.position.x + player.width <= canvas.width
+  ) {
+    player.velocity.x = 5;
+    player.rotation = 0.15;
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0;
+  }
 }
 
-animate()
+animate();
 
 //movement for press down
-addEventListener("keydown", ({key}) => {
-    switch(key){
-        case "a":
-            keys.a.pressed = true
-            break
-        case "s":
-            keys.s.pressed = true
-            break
-        case "d":
-            keys.d.pressed = true
-            break
-        case "w":
-            keys.w.pressed = true
-            break
-        case "space":
-            keys.w.pressed = true
-            break
-    }
-})
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "s":
+      keys.s.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "w":
+      keys.w.pressed = true;
+      break;
+    case " ":
+      keys.w.pressed = true;
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      break;
+  }
+});
 
 //stop moving on press up
-addEventListener("keyup", ({key}) => {
-    switch(key){
-        case "a":
-            console.log("a pressed down")
-            keys.a.pressed = false
-            break
-        case "s":
-            keys.s.pressed = false
-            break
-        case "d":
-            keys.d.pressed = false
-            break
-        case "w":
-            keys.w.pressed = false
-            break
-    }
-})
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case " ":
+      console.log(projectiles);
+      keys.w.pressed = false;
+      break;
+  }
+});
